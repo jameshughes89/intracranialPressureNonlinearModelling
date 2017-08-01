58f1f328454b06c40fcf68e33dbe6b7dc7c0903b
James Hughes <JamesHughes89@Gmail.com>
2017-08-01 10:54:49 -0400

    add the script (2) to do find the top models. Also add the top models python file that contains the actual top models

---
+++ scripts-Group/bestExpressions_1s.py
@@ -0,0 +1,9 @@
+from math import *
+
+def func_0(v0,v1,v2,v3,v4,v5,v6): return  ( ( ( ( v5 *sin( v1 )) - v5 ) / -5.655661925125353 ) +sin( (tan(sin(tan( v5 )))+abs( ( v5 *sin( v1 )) )) )) 
+def func_1(v0,v1,v2,v3,v4,v5,v6): return cos(exp( (abs( ( ( ( v0 / 5.282228461388399 ) / v0 ) * v0 ) )-sin( v5 )) ))
+def func_2(v0,v1,v2,v3,v4,v5,v6): return  ( v5 /exp(abs( ( ( v0 * v1 ) /exp( v1 )) ))) 
+
+funcs = [func_0,func_1,func_2]
+
+def getFuncs(): return funcs
---
+++ scripts-Group/bestExpressions_250.py
@@ -0,0 +1,9 @@
+from math import *
+
+def func_0(v0,v1,v2,v3,v4,v5,v6): return  (cos( ( ( -1.3126969241189812 - ( v1 *exp( ( -1.3126969241189812 +sin( v2 )) )) ) +tan(sin( v5 ))) )+ ( ( v1 + (sin( v1 )+sin( v5 )) ) / ( ( -4.29803949806435 +tan(sin( v5 ))) /tan(sin( v5 ))) ) ) 
+def func_1(v0,v1,v2,v3,v4,v5,v6): return sin( (cos(sin( v1 ))* ( ( (sin( ( v1 + ( v3 + 6.990432058235669 ) ) )-sin( v1 )) *cos( ( v5 - 6.990432058235669 ) )) + v5 ) ) )
+def func_2(v0,v1,v2,v3,v4,v5,v6): return  ( (exp( ( ( ( ( v3 * ( ( 2.135808177448986 - v1 ) -cos( v1 )) ) - 4.512216897378863 ) /abs( ( ( 2.135808177448986 - v1 ) -cos( v1 )) )) *abs( v5 )) )+ v5 ) *cos( ( ( ( 2.135808177448986 - v1 ) -cos( v1 )) / 2.135808177448986 ) )) 
+
+funcs = [func_0,func_1,func_2]
+
+def getFuncs(): return funcs
